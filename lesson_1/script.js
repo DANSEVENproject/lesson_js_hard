d826e486c08440efb126464378e31cbe9441c06e
Daniel <KunitskiiDanik@mail.ru>
2021-11-08 17:17:34 +0300

    lesson_1

---
+++ lesson_1/script.js
@@ -0,0 +1,8 @@
+let money = 100;
+let income = "more";
+let addExpenses = 500;
+const deposit = 5000
+const mission = 12;
+const period = "1 hour";
+alert("deposit: " + deposit);
+console.log("I have " + money + " money");
