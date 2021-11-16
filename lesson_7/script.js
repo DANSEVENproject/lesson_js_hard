1809f6d364601272479657189753dfd7f8de3c0d
Daniel <KunitskiiDanik@mail.ru>
2021-11-16 22:45:40 +0300

    add lesson_7

---
+++ lesson_7/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const week = [
+    ['Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Saturday', 'Sunday'],
+    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+];
+
+let personName = prompt('Напишите Ваше имя:');
+
+personName === 'Артём' ? (
+    console.log('директор')
+) : (
+    personName === 'Максим' ? (
+        console.log('преподаватель')
+    ) : (
+        console.log('студент')
+    )
+)
+var now = new Date();
+
+let lang = prompt('Какой язык вы хотите поставить?(ru/en)');
+if (lang === 'en') {
+    for (let i = 0; i < week.length - 1; i++) {
+        for (let j = 0; j < 7; j++) {
+            if (now.getDay() === j + 1) {
+                document.writeln(week[i][j].bold());
+                document.write('<br>');
+            } else {
+                document.writeln(week[i][j].italics());
+                document.write('<br>');
+            }
+        }
+    }
+} else {
+    if (lang === 'ru') {
+        for (let i = 1; i < week.length; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (now.getDay() === j + 1) {
+                    document.writeln(week[i][j].bold());
+                } else {
+                    document.writeln(week[i][j].italics());
+                    document.write('<br>');
+                }
+            }
+        }
+    } else {
+        console.log('ЧЕЛ ТЫ...')
+    }
+}
