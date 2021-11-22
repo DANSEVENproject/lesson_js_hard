173872cd253184c37142a7e540e802f700876bf2
Daniel <KunitskiiDanik@mail.ru>
2021-11-22 12:51:09 +0300

    correct lesson_10

--- lesson_10/script.js
+++ lesson_10/script.js
@@ -1,7 +1,7 @@
 const buttonLi = document.querySelector('button');
-let ulLi = document.querySelector('ul');
+const ulLi = document.querySelector('ul');
 
 buttonLi.onclick = function() {
-    let inputLi = document.querySelector('input').value;
+    const inputLi = document.querySelector('input').value;
     ulLi.insertAdjacentHTML('beforeend', '<li>' + inputLi + '</li>');
 }
