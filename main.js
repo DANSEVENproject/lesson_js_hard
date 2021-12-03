b25df308efe72863404a5b82f6269158b4a9d04d
Daniel <KunitskiiDanik@mail.ru>
2021-12-03 21:53:40 +0300

    add lesson_15

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ class First {
     }
 }
 class Second extends First {
+    constructor() {
+        super();
+    }
+
     hey() {
         this.hello();
         console.log(`А я наследуемый метод!`);
     }
 }
-const fisrt = new First();
 const second = new Second();
 second.hey();
